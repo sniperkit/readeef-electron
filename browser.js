5e31fc515fb44dc4551d28f96eb8d77faa82d076
Viktor Kojouharov <developer@sugr.org>
2017-12-25 12:37:34 +0200

    Update the electron client for the new readeef

--- browser.js
+++ browser.js
@@ -12,52 +12,11 @@ if (!result || !result.success || !result.value) {
 urlParser.href = result.value;
 const host = urlParser.hostname;
 
-var notification;
-result = JSON.parse(ipcRenderer.sendSync('get-config-item-sync', 'show-notifications'));
-if (!result || !result.success || result.value === null || result.value) {
-	window.addEventListener('updates-available', debounce(function(event) {
-		var result = JSON.parse(ipcRenderer.sendSync('main-window-is-focused'));
-		if (!result || !result.success || result.value) {
-			return;
-		}
-
-		var opts = { 'body': 'Feed "' + event.detail.title + '" has been updated' };
-
-		result = JSON.parse(ipcRenderer.sendSync('get-icon-uri'));
-		if (result && result.success && result.value) {
-			opts.icon = result.value;
-		}
-
-		if (notification) {
-			notification.close();
-		}
-
-		notification = new Notification('New articles', opts);
-
-		notification.onclick = function() {
-			ipcRenderer.send('focus-main-window');
-		};
-	}, 5000));
-}
-
-window.addEventListener('pre-article-open', function(event) {
-	event.preventDefault();
-	shell.openExternal(event.detail.link);
+window.addEventListener("load", function() {
+	document.body.addEventListener('open-link', function(event) {
+		event.preventDefault();
+		shell.openExternal(event.detail);
+	});
 });
 
 window.moment = moment;
-
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
